cc69d8d344a944537fb96ded9a8bc2fe6813de42
Hammad <84343655+hammadrepository@users.noreply.github.com>
2022-12-22 15:53:18 +0000

    Refactor

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 
 Broadcast::routes(['middleware' => ['auth:sanctum']]);
@@ -28,7 +28,6 @@ Broadcast::routes(['middleware' => ['auth:sanctum']]);
 //    $pusher = new Pusher\Pusher(env('PUSHER_APP_KEY'), env('PUSHER_APP_SECRET'), env('PUSHER_APP_ID'));
 //    return $pusher->socket_auth('private-groups.1', $request->request->get('socket_id'));
 //});
-
 
 Route::namespace('API')->group(function () {
     Route::post('/register', 'UserController@register');
@@ -38,33 +37,33 @@ Route::namespace('API')->group(function () {
     Route::middleware('auth:sanctum')->group(function (){
         Route::get('/questions','QuestionController@index');
         Route::post('/questions','QuestionController@storeQuestionAnswer');
+
+        Route::get('/conversation/history/{id}', 'ConversationController@loadChat')->name('conversation.history');
+        Route::post('/banUser', 'UserController@banUser')->name('banUser');
+
+        Route::post('/session/create', 'LiveSessionController@create');
+        Route::get('/sessions', 'LiveSessionController@index');
+        Route::get('/sessions/users', 'LiveSessionController@userSessions');
+        Route::post('/session/update', 'LiveSessionController@update')->name('session.update');
+        Route::get('/session/destroy/{id}', 'LiveSessionController@destroy')->name('session.delete');
+        Route::get('/session/groups', 'LiveSessionController@getAllGroups');
+
+        Route::post('conversation/sendFile','ConversationController@sendFile');
+
+        Route::get('/documents', 'DocumentController@index');
+        Route::post('document-upload', 'DocumentController@store');
+        Route::delete('document/{id}', 'DocumentController@destroy');
+        Route::apiResource('conversations', 'ConversationController');
+        Route::post('/otp/verify','UserController@verifyOtp')->name('otp.verify');
+        Route::post('/otp/resend','UserController@resendOtp')->name('otp.resend');
+        Route::apiResources(['user' => 'UserController']);
+
+        Route::get('/profile','UserController@current_user');
+        Route::put('/profiles/{id}','UserController@profile_update');
+        Route::get('user/{id}','UserController@show');
+        Route::post('conversations','ConversationController@store');
     });
 
-    Route::get('user/{id}','UserController@show');
-    Route::get('/deleteToken/{email}','UserController@deleteToken');
-
-    Route::post('conversations','ConversationController@store');
     Route::post('/contact','ContactController@contactUs');
 });
-Route::get('/conversation/history/{id}', 'API\ConversationController@loadChat')->name('conversation.history');
-Route::post('/banUser', 'API\UserController@banUser')->name('banUser');
 
-Route::post('/session/create', 'API\LiveSessionController@create');
-Route::get('/sessions', 'API\LiveSessionController@index');
-Route::get('/sessions/users', 'API\LiveSessionController@userSessions');
-Route::post('/session/update', 'API\LiveSessionController@update')->name('session.update');
-Route::get('/session/destroy/{id}', 'API\LiveSessionController@destroy')->name('session.delete');
-Route::get('/session/groups', 'API\LiveSessionController@getAllGroups');
-
-Route::post('conversation/sendFile','API\ConversationController@sendFile');
-
-Route::get('/documents', 'API\DocumentController@index');
-Route::post('document-upload', 'API\DocumentController@store');
-Route::delete('document/{id}', 'API\DocumentController@destroy');
-Route::apiResource('conversations', 'API\ConversationController');
-Route::post('/otp/verify','API\UserController@verifyOtp')->name('otp.verify');
-Route::post('/otp/resend','API\UserController@resendOtp')->name('otp.resend');
-Route::apiResources(['user' => 'API\UserController']);
-
-Route::get('/profile','API\UserController@current_user');
-Route::put('/profiles/{id}','API\UserController@profile_update');
